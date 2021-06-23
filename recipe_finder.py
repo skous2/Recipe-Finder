09f8512158cb05ea8c4d5c098b2f4ddf92099dc0
skous2 <tanner.skousen@gmail.com>
2021-06-23 13:03:47 -0400

    Update recipe_finder.py

--- recipe_finder.py
+++ recipe_finder.py
@@ -29,6 +29,11 @@ def convert(row):
     #print(row)
     return '<a href="{0}" target="_blank">{0}</a>'.format(row)
 
+st.markdown(""" div.stButton > button:first-child {
+background-color: #00cc00;color:#FF0066;font-size:20px;height:3em;width:30em;border-radius:10px 10px 10px 10px;
+}
+""", unsafe_allow_html=True)
+
 
 
 
